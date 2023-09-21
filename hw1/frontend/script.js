529de074189baaa7a24f1b17da0289c2b138a82e
JasonLin1000 <abstract@LAPTOP-Jasonlin>
2023-09-21 19:19:39 +0800

    0921 1953

--- hw1/frontend/script.js
+++ hw1/frontend/script.js
@@ -1,13 +1,13 @@
 /* global axios */
 const itemGlanceTemplate = document.querySelector("#diary-glance-template");
-const itemViewTemplate = document.querySelector("#diary-view-template");
 const diaryList = document.querySelector("#diaries");
-const diaryShow = document.querySelector("#show-diary")
+const diaryShow = document.querySelector("#show-diary");
+const diaryModify = document.querySelector("#modify-diary");
 
 const instance = axios.create({
   baseURL: "http://localhost:8000/api",
 });
-let isDisplay = false;
+let isShow = false;
 let isModify = false;
 
 async function main() {
@@ -21,43 +21,89 @@ async function main() {
     alert("Failed to load diaries!");
   }
 }
+//檢測現在是要檢視/編輯，並正確顯示
 function checkRender(){
-  if(isDisplay){
+  if(isShow){
     diaryList.style.display='none';
     diaryShow.style.display='';
+    diaryModify.style.display='none';
   }
   else{
+    diaryList.style.display='';
+    diaryShow.style.display='none';
+    diaryModify.style.display='none';
+    setupEventListenersForAdd();
+  }
+  if(isModify){
     diaryList.style.display='none';
-    diaryShow.style.display='';
+    diaryShow.style.display='none';
+    diaryModify.style.display='';
+  }
+  else{
+    diaryList.style.display='';
+    diaryShow.style.display='none';
+    diaryModify.style.display='none';
+    setupEventListenersForAdd();
   }
 }
-function setupEventListeners() {
+
+function setupEventListenersForAdd(){
   const addDiaryButton = document.querySelector("#diary-add");
+  addDiaryButton.addEventListener("click", () => {
+    isModify = true;
+    isShow = false;
+    const setDate = document.querySelector("#modify-diary .date");
+    setDate.value=formatTime();
+  });
+}
+
+function setupEventListenersForView(){
+  diaryList.forEach((diary)=>{
+    diary.addEventListener("click",()=>{
+      isShow=true;
+      isModify=false;
+      const setDate = document.querySelector("#show-diary .date");
+      const setTag = document.querySelector("#show-diary .tag");
+      const setMood = document.querySelector("#show-diary .mood");
+      const setDisc = document.querySelector(".diary-description-show");
+      setDate.value = diary.date;
+      setTag.value = diary.tag;
+      setMood.value = diary.mood;
+      setDisc.value = diary.discription;
+    })
+  })
+}
+
+function modifyPage() {
   const confirmAddButton = document.querySelector("#confirm");
   const cancelButton = document.querySelector("#cancel");
-  const diaryInput = document.querySelector("#diary-input");
+  const diaryDate = document.querySelector("#modify-diary .date");
+  const diaryTag = document.querySelector("#tag-form");
+  const diaryMood = document.querySelector("#mood-form");
   const diaryDescriptionInput = document.querySelector(
     "#diary-description-input",
   );
-  addDiaryButton.addEventListener("click", async () => {
-    const title = diaryInput.value;
+  cancelButton.addEventListener("click",()=>{
+    isShow=false;
+    isModify=false;
+  });
+  confirmAddButton.addEventListener("click", async () => {
+    const date = diaryDate.value;
     const description = diaryDescriptionInput.value;
-    if (!title) {
-      alert("Please enter a diary title!");
-      return;
-    }
+    const tag = diaryTag.value;
+    const mood = diaryMood.value;
     if (!description) {
       alert("Please enter a diary description!");
       return;
     }
     try {
-      const diary = await createDiary({ title, description });
+      const diary = await createDiary({ description,date,tag,mood });
       renderDiary(diary);
     } catch (error) {
       alert("Failed to create diary!");
       return;
     }
-    diaryInput.value = "";
+    diaryDate.value="";
     diaryDescriptionInput.value = "";
   });
 }
